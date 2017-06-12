153b36a7647a245672ec603a154bde4dd0e6677e
Ömer Günal <o.gunal977@gmail.com>
2017-06-12 19:00:43 +0300

    Update helper.py

--- modules/helper.py
+++ modules/helper.py
@@ -1,6 +1,6 @@
 def Features(start="\033[1m", mid="\033[93m", end="\033[0;0m", arrow=" --> "):
     func_dict = {
-        "Twitter account analysis": "!twitter @ussername",
+        "Twitter account analysis": "!twitter @username",
         "URL scan": "!urlscan <URL-TO-SCAN>",
         "File scan": "!filescan <PATH-TO-FILE>",
         "Linux enumeration": "!linuxenum",
